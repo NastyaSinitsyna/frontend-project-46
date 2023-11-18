56a5795b367db843da4aec24cb4fb48ac56d4d57
Nastya Sinitsyna <nastasya2507@gmail.com>
2023-11-18 18:11:14 +0500

    format option and arguments added to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,8 @@ import { program } from 'commander';
 program
   .description(`Compares two configuration files and shows a difference.`)
   .version('0.0.1', '-V, --version', 'output the current version')
-  .usage("[options]");
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
   
 program.parse();
 
