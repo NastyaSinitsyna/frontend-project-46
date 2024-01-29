f16f3542a526ecf9d0a639c1df1a89fa3fedd506
Nastya Sinitsyna <nastasya2507@gmail.com>
2024-01-29 11:59:00 +0500

    Add file reading and parsing

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,24 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import fs from 'node:fs';
+import path from 'node:path';
+
+import fileParse from './parse.js';
 
 program
   .description(`Compares two configuration files and shows a difference.`)
   .version('0.0.1', '-V, --version', 'output the current version')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format');
-  
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    const absFilepath1 = path.resolve(process.cwd(), filepath1);
+    const absFilepath2 = path.resolve(process.cwd(), filepath2);
+
+    const file1Content = fileParse(fs.readFileSync(absFilepath1, 'utf8'));
+    console.log({ file1Content });
+    const file2Content = fileParse(fs.readFileSync(absFilepath2, 'utf8'));
+    console.log({ file2Content });
+  });
+
 program.parse();
-
-
