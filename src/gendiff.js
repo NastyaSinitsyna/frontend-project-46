722d8419be76fb1441c2203a40fa293027dd6d56
Nastya Sinitsyna <nastasya2507@gmail.com>
2024-07-28 14:16:39 +0500

    add stylish formater

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 
 import fileParse from './parsers.js';
 
+import stylish from './stylish.js';
+
 export default (filepath1, filepath2) => {
   const content1 = fileParse(filepath1);
   const content2 = fileParse(filepath2);
@@ -9,45 +11,25 @@ export default (filepath1, filepath2) => {
   const keys1 = Object.keys(content1).sort();
   const keys2 = Object.keys(content2).sort();
 
-  const content1Sorted = keys1.reduce((acc, key1) => {
+  const sortedContent1 = keys1.reduce((acc, key1) => {
     acc[key1] = content1[key1];
     return acc;
   }, {});
-  const content2Sorted = keys2.reduce((acc, key2) => {
+  const sortedContent2 = keys2.reduce((acc, key2) => {
     acc[key2] = content2[key2];
     return acc;
   }, {});
-
   const diffKeys = _.union(keys1, keys2);
 
   const diff = diffKeys.reduce((acc, diffKey) => {
-    const value1 = content1Sorted[diffKey];
-    const value2 = content2Sorted[diffKey];
-    if (value1 === value2) {
-      const key = `  ${diffKey}`;
-      acc[key] = value1;
-      return acc;
-    } if (!value2) {
-      const key = `- ${diffKey}`;
-      acc[key] = value1;
-      return acc;
-    } if (!value1) {
-      const key = `+ ${diffKey}`;
-      acc[key] = value2;
-      return acc;
-    }
-    const key1 = `- ${diffKey}`;
-    acc[key1] = value1;
-    const key2 = `+ ${diffKey}`;
-    acc[key2] = value2;
+    const value1 = sortedContent1[diffKey];
+    const value2 = sortedContent2[diffKey];
+    acc.push([diffKey, value1, value2]);
     return acc;
-  }, {});
+  }, []);
+  console.log(diff);
 
-  const entries = Object.entries(diff);
-  const diffAsString = entries.reduce((acc, [key, value]) => {
-    const newAcc = acc.concat(`  ${key}: ${value}\n`);
-    return newAcc;
-  }, '');
-  const diffFormatted = `{\n${diffAsString}}`;
-  return diffFormatted;
+  const formattedDiff = stylish(diff);
+
+  return formattedDiff;
 };
---
+++ src/stylish.js
@@ -0,0 +1,59 @@
+const diffToObj = (data) => {
+  const diffAsObj = data.reduce((acc, [key, value1, value2]) => {
+    let specSymbol;
+    if (value1 === value2) {
+      specSymbol = '  ';
+      acc[`${specSymbol}${key}`] = value1;
+      return acc;
+    } if (!value2) {
+      specSymbol = '- ';
+      acc[`${specSymbol}${key}`] = value1;
+      return acc;
+    } if (!value1) {
+      specSymbol = '+ ';
+      acc[`${specSymbol}${key}`] = value2;
+      return acc;
+    }
+    specSymbol = '- ';
+    acc[`${specSymbol}${key}`] = value1;
+    specSymbol = '+ ';
+    acc[`${specSymbol}${key}`] = value2;
+    return acc;
+  }, {});
+  return diffAsObj;
+};
+
+const stringify = (data, replacer = ' ', spacesCount = 4) => {
+  const iter = (currentData, level) => {
+    const dataType = typeof currentData;
+    if (dataType !== 'object' || currentData === null) {
+      const result = `${currentData}`;
+      return result;
+    }
+    if (Array.isArray(currentData)) {
+      const result = `[${Array.toString(currentData)}]`;
+      return result;
+    }
+
+    const entries = Object.entries(currentData);
+    const indentSize = spacesCount * level;
+    let currentIndent;
+    let bracketIndent;
+    const result = entries.reduce((acc, [key, value]) => {
+      if (key.startsWith('  ') || key.startsWith('+ ') || key.startsWith('- ')) {
+        currentIndent = replacer.repeat(indentSize - 2);
+        bracketIndent = replacer.repeat(indentSize - spacesCount);
+      } else {
+        currentIndent = replacer.repeat(indentSize);
+        bracketIndent = replacer.repeat(indentSize - spacesCount);
+      }
+      const newAcc = `${acc}\n${currentIndent}${key}: ${iter(value, level + 1)}`;
+      return newAcc;
+    }, '');
+    return `{${result}\n${bracketIndent}}`;
+  };
+
+  return iter(data, 1);
+};
+
+export default (data) => stringify(diffToObj(data));
