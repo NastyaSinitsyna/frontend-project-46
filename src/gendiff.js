f9a43935d51b8d2277e03b12216b7d9aa8f7e6fd
Nastya Sinitsyna <nastasya2507@gmail.com>
2024-09-24 11:05:05 +0500

    fix linter issues in gendiff func

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,20 +3,11 @@ import _ from 'lodash';
 export default (content1, content2) => {
   const keys1 = Object.keys(content1);
   const keys2 = Object.keys(content2);
-  const sortedContent1 = keys1.reduce((acc, key1) => {
-    acc[key1] = content1[key1];
-    return acc;
-  }, {});
-  const sortedContent2 = keys2.reduce((acc, key2) => {
-    acc[key2] = content2[key2];
-    return acc;
-  }, {});
-  const diffKeys = _.union(keys1, keys2).sort();
+  const diffKeys = _.sortBy(_.union(keys1, keys2));
   const diff = diffKeys.reduce((acc, diffKey) => {
-    const preValue = sortedContent1[diffKey];
-    const curValue = sortedContent2[diffKey];
-    acc[`${diffKey}Diff`] = { diffKey, preValue, curValue };
-    return acc;
+    const preValue = content1[diffKey];
+    const curValue = content2[diffKey];
+    return { ...acc, [`${diffKey}Diff`]: { diffKey, preValue, curValue } };
   }, {});
   return diff;
 };
