eab27d17887f5aa40936cfdc85a3eead0f6097a3
Nastya Sinitsyna <nastasya2507@gmail.com>
2024-10-08 12:34:13 +0500

    fix parsers in side-effects

--- src/parsers.js
+++ src/parsers.js
@@ -2,12 +2,17 @@ import fs from 'node:fs';
 import path from 'node:path';
 import yaml from 'js-yaml';
 
-export default (filepath) => {
+const getFileData = (filepath) => {
   const absFilepath = path.resolve(process.cwd(), filepath);
   const extention = path.extname(absFilepath);
   const content = fs.readFileSync(absFilepath, 'utf8');
-  if (extention.includes('json')) {
-    return JSON.parse(content);
+  return { content, extention };
+};
+
+export default (filepath) => {
+  const fileData = getFileData(filepath);
+  if (fileData.extention.includes('json')) {
+    return JSON.parse(fileData.content);
   }
-  return yaml.load(content);
+  return yaml.load(fileData.content);
 };
