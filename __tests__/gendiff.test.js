be49ce6646a5f05bfa7d3c88c6aef97bed3841f9
Nastya Sinitsyna <nastasya2507@gmail.com>
2024-10-09 11:20:53 +0500

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,6 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import { expect, test } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 import url from 'url';
 import gendiff from '../index.js';
 
@@ -8,36 +8,76 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFileContent = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
 const jsonFilepath1 = getFixturePath('file1.json');
 const jsonFilepath2 = getFixturePath('file2.json');
 const yamlFilepath1 = getFixturePath('file1.yml');
 const yamlFilepath2 = getFixturePath('file2.yml');
 
-const stylishDiffPath = getFixturePath('expected.stylish.diff.txt');
-const plainDiffPath = getFixturePath('expected.plain.diff.txt');
-const jsonDiffPath = getFixturePath('expected.json.diff.txt');
+const expectedStylishDiff = getFileContent('expected.stylish.diff.txt');
+const expectedPlainDiff = getFileContent('expected.plain.diff.txt');
+const expectedJsonDiff = getFileContent('expected.json.diff.txt');
 
-test('should return diff in stylish format', () => {
-  const diffJson = gendiff(jsonFilepath1, jsonFilepath2, 'stylish');
-  const diffYAML = gendiff(yamlFilepath1, yamlFilepath2, 'stylish');
-  const expectedStylishDiff = fs.readFileSync(stylishDiffPath, 'utf8');
-  expect(diffJson).toBe(expectedStylishDiff);
-  expect(diffYAML).toBe(expectedStylishDiff);
+describe.each([
+  {
+    a: jsonFilepath1,
+    b: jsonFilepath2,
+    expected: expectedStylishDiff,
+  },
+  {
+    a: yamlFilepath1,
+    b: yamlFilepath2,
+    expected: expectedStylishDiff,
+  },
+])('gendiff in stylish format', ({ a, b, expected }) => {
+  test('should return stylish diff for json files', () => {
+    expect(gendiff(a, b, 'stylish')).toBe(expected);
+  });
+
+  test('should return stylish diff for yaml files', () => {
+    expect(gendiff(a, b, 'stylish')).toBe(expected);
+  });
 });
 
-test('should return diff in plain format', () => {
-  const diffJson = gendiff(jsonFilepath1, jsonFilepath2, 'plain');
-  const diffYAML = gendiff(yamlFilepath1, yamlFilepath2, 'plain');
-  const expectedPlainDiff = fs.readFileSync(plainDiffPath, 'utf8');
-  expect(diffJson).toBe(expectedPlainDiff);
-  expect(diffYAML).toBe(expectedPlainDiff);
+describe.each([
+  {
+    a: jsonFilepath1,
+    b: jsonFilepath2,
+    expected: expectedPlainDiff,
+  },
+  {
+    a: yamlFilepath1,
+    b: yamlFilepath2,
+    expected: expectedPlainDiff,
+  },
+])('gendiff in plain format', ({ a, b, expected }) => {
+  test('should return plain diff for json files', () => {
+    expect(gendiff(a, b, 'plain')).toBe(expected);
+  });
+
+  test('should return plain diff for yaml files', () => {
+    expect(gendiff(a, b, 'plain')).toBe(expected);
+  });
 });
 
-test('should return diff in json format', () => {
-  const diffJson = gendiff(jsonFilepath1, jsonFilepath2, 'json');
-  const diffYAML = gendiff(yamlFilepath1, yamlFilepath2, 'json');
-  const expectedJsonDiff = fs.readFileSync(jsonDiffPath, 'utf8');
-  expect(diffJson).toBe(expectedJsonDiff);
-  expect(diffYAML).toBe(expectedJsonDiff);
+describe.each([
+  {
+    a: jsonFilepath1,
+    b: jsonFilepath2,
+    expected: expectedJsonDiff,
+  },
+  {
+    a: yamlFilepath1,
+    b: yamlFilepath2,
+    expected: expectedJsonDiff,
+  },
+])('gendiff in json format', ({ a, b, expected }) => {
+  test('should return json diff for json files', () => {
+    expect(gendiff(a, b, 'json')).toBe(expected);
+  });
+
+  test('should return json diff for yaml files', () => {
+    expect(gendiff(a, b, 'json')).toBe(expected);
+  });
 });
