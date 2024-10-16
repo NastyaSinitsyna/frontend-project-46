6674a3f5561900491cf027a02aa5f99b5576243e
Nastya Sinitsyna <nastasya2507@gmail.com>
2024-10-16 16:55:32 +0500

    fix gendiff function

--- src/gendiff.js
+++ src/gendiff.js
@@ -4,22 +4,22 @@ const gendiff = (content1, content2) => {
   const keys1 = Object.keys(content1);
   const keys2 = Object.keys(content2);
   const diffKeys = _.sortBy(_.union(keys1, keys2));
-  const diff = diffKeys.map((diffKey) => {
-    const preValue = content1[diffKey];
-    const curValue = content2[diffKey];
+  const diff = diffKeys.map((key) => {
+    const preValue = content1[key];
+    const curValue = content2[key];
     if (typeof curValue === 'object' && typeof preValue === 'object') {
-      return { [`${diffKey}`]: gendiff(preValue, curValue) };
+      return { key, status: 'nested', children: gendiff(preValue, curValue) };
     }
     if (preValue === undefined) {
-      return { [`${diffKey}`]: { status: 'added', preValue, curValue } };
+      return { key, status: 'added', curValue };
     }
     if (curValue === undefined) {
-      return { [`${diffKey}`]: { status: 'removed', preValue, curValue } };
+      return { key, status: 'removed', preValue };
     }
     if (preValue !== curValue) {
-      return { [`${diffKey}`]: { status: 'changed', preValue, curValue } };
+      return { key, status: 'changed', preValue, curValue };
     }
-    return { [`${diffKey}`]: { status: 'unchanged', preValue, curValue } };
+    return { key, status: 'unchanged', curValue };
   });
   return diff;
 };
